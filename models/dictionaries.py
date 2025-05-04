e96a4c2aff475b11afde8eec270f3dcdc211b9e1
shinny0wl <paulina.floress@iteso.mx>
2025-05-04 13:53:38 -0600

    Datos en csv

---
+++ models/dictionaries.py
@@ -0,0 +1,39 @@
+
+# de cliente
+estadoCuenta = {
+    1: "Activo",
+    2: "Inactivo",
+    3: "Suspendido"
+}
+
+# Agente
+estadoEnEmpresa = {
+    1: "Activo",
+    2: "Inactivo",
+    3: "Incapacitado"
+}
+
+# ticket
+tipoProblema = {
+    1: "Problemas de inicio de sesión ",
+    2: "Errores en el sistema o software",
+    3: "Solicitud de instalación o actualización",
+    4: "Problemas con hardware",
+    5: "Recuperación de datos o archivos perdidos ",
+    6: "Problemas con correo electrónico    ",
+    7: "Acceso restringido o permisos insuficientes",
+    8: "Consultas o capacitación sobre el sistema"
+}
+
+prioridad = {
+    1: "Baja",
+    2: "Media",
+    3: "Alta",
+    4: "Crítica"
+}
+
+estado = {
+    1: "Abierto",
+    2: "En Proceso",
+    3: "Cerrado"
+}
