7bf5223980c1129bb2e49b57f2b724f2e0caef61
Jaime Enrique Galindo Villegas <jaime.galindo@iteso.mx>
2025-05-05 17:40:21 -0600

    Reorden en carpeta models

---
+++ models/Mongo/users.py
@@ -0,0 +1,11 @@
+# Poblado de usuarios
+
+import conection
+
+users_types = {
+    "name": str,
+    "email": str,
+    "phone": str,
+    "account_status": str,
+    "tickets_history": list
+}
--- models/conection.py
+++ models/conection.py
@@ -1,6 +1,6 @@
-from cassandra.cluster import Cluster # type: ignore
-from pymongo import MongoClient # type: ignore
-import models.model as model
+from cassandra.cluster import Cluster
+from pymongo import MongoClient 
+# import models.model as model Descomentar Cuando esté la conexión a Cassandra
 import os
 
 # Read env vars releated to Cassandra App
@@ -8,12 +8,12 @@ CLUSTER_IPS = os.getenv('CASSANDRA_CLUSTER_IPS', 'localhost')
 KEYSPACE = os.getenv('CASSANDRA_KEYSPACE', 'customer_support')
 REPLICATION_FACTOR = os.getenv('CASSANDRA_REPLICATION_FACTOR', '1')
 
-def connect_cassandra():
-    cluster = Cluster(['127.0.0.1'])  # Dirección del contenedor
-    session = cluster.connect()
-    model.create_keyspace(session, KEYSPACE, REPLICATION_FACTOR)
-    session.set_keyspace(KEYSPACE)
-    return session
+# def connect_cassandra():
+#     cluster = Cluster(['127.0.0.1'])  # Dirección del contenedor
+#     session = cluster.connect()
+#     model.create_keyspace(session, KEYSPACE, REPLICATION_FACTOR)
+#     session.set_keyspace(KEYSPACE)
+#     return session
 
 def connect_mongodb():
     client = MongoClient('mongodb://localhost:27017/')
@@ -21,7 +21,7 @@ def connect_mongodb():
     return db
 
 if __name__ == "__main__":
-    cassandra_session = connect_cassandra()
+    # cassandra_session = connect_cassandra()
     mongo_db = connect_mongodb()
     print("Conexión establecida con Cassandra y MongoDB")
 
