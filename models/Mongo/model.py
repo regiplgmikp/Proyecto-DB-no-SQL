4b318cb53cb66c5f0ce76d4bbf6638fea60912fd
Jaime Enrique Galindo Villegas <jaime.galindo@iteso.mx>
2025-05-09 13:09:58 -0600

    Método para insertar una empresa en model.py agregado

--- models/Mongo/model.py
+++ models/Mongo/model.py
@@ -1,5 +1,6 @@
 import models.conection as conection
 from .Agente import Agente
+from .Empresa import Empresa
 from bson import Binary
 
 db = conection.connect_mongodb()
@@ -8,7 +9,12 @@ if 'agentes' not in db.list_collection_names():
     db.create_collection('agentes')
 
 # Creación de indices
+# Agentes:
 db.agentes.create_index('idAgente', unique=True)
+
+# Empresas:
+db.empresas.create_index('idEmpresa', unique=True)
+
 
 def insertar_agente(agente):
     try:
@@ -21,5 +27,18 @@ def insertar_agente(agente):
         agente_dict['idEmpresa'] = Binary.from_uuid(agente_dict['idEmpresa'])
         
         collection.insert_one(agente_dict)
+    except Exception as e:
+        raise e
+
+def insertar_empresa(empresa):
+    try:
+        collection = db['agentes']
+        empresa = Empresa.crear_desde_dict(empresa)
+        
+        # Convertir UUIDs a Binary para MongoDB
+        empresa_dict = empresa.model_dump(by_alias=True)
+        empresa_dict['iEmpresa'] = Binary.from_uuid(empresa_dict['idEmpresa'])
+        
+        collection.insert_one(empresa_dict)
     except Exception as e:
         raise e
