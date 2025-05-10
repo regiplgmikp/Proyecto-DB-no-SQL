15098cb61785cfcf61517df954ea6af5b548ddef
Jaime Enrique Galindo Villegas <jaime.galindo@iteso.mx>
2025-05-10 13:05:03 -0600

    Agregación de obtener entidades por id en el modelo de mongo

--- models/Mongo/MongoModel.py
+++ models/Mongo/MongoModel.py
@@ -28,16 +28,32 @@ class MongoModel:
         cls._insertar_documento('agentes', agente, Agente)
 
     @classmethod
+    def obtener_agente_por_id(cls, idAgente: UUID):
+        return cls._obtener_documento_por_id('agentes', idAgente, 'idAgente')
+
+    @classmethod
     def insertar_empresa(cls, empresa):
         cls._insertar_documento('empresas', empresa, Empresa)
+
+    @classmethod
+    def obtener_empresa_por_id(cls, idEmpresa: UUID):
+        return cls._obtener_documento_por_id('empresas', idEmpresa, 'idEmpresa')
 
     @classmethod
     def insertar_cliente(cls, cliente):
         cls._insertar_documento('clientes', cliente, Cliente)
 
     @classmethod
+    def obtener_cliente_por_id(cls, idCliente: UUID):
+        return cls._obtener_documento_por_id('clientes', idCliente, 'idCliente')
+
+    @classmethod
     def insertar_ticket(cls, ticket):
         cls._insertar_documento('tickets', ticket, Ticket)
+
+    @classmethod
+    def obtener_ticket_por_id(cls, idTicket: UUID):
+        return cls._obtener_documento_por_id('tickets', idTicket, 'idTicket')
 
     @classmethod
     def _insertar_documento(cls, collection_name: str, data: dict, model_class: Type[Union[Agente, Empresa, Cliente, Ticket]]):
@@ -54,3 +70,20 @@ class MongoModel:
             collection.insert_one(data_dict)
         except Exception as e:
             raise e
+
+
+    @classmethod
+    def _obtener_documento_por_id(cls, collection_name: str, id_value: UUID, id_field: str):
+        collection = cls.db[collection_name]
+        documento = collection.find_one({id_field: Binary.from_uuid(id_value)})
+
+        if documento:
+            # Convertir todos los campos que sean Binary a UUID
+            for key, value in documento.items():
+                if isinstance(value, Binary):
+                    documento[key] = UUID(bytes=value)
+
+            # Eliminar el campo '_id' para evitar confusión
+            documento.pop('_id', None)
+
+        return documento
